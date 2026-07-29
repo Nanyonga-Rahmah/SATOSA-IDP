9bc5f1aeae3c820614970f51efd3face470819ed
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2026-07-29 09:05:03 +0300

    fix: replace localhost urls with deployed urls

--- src/idp/config.py
+++ src/idp/config.py
@@ -7,7 +7,7 @@ from saml2 import BINDING_HTTP_POST, BINDING_HTTP_REDIRECT
 _PROJECT_ROOT: Path = Path(__file__).resolve().parents[0]
 
 CONFIG: dict[str, object] = {
-    "entityid": "http://localhost:9000/idp",
+    "entityid": "https://idp-latest.onrender.com/idp",
     "key_file": str(_PROJECT_ROOT / "certs" / "idp.key"),
     "cert_file": str(_PROJECT_ROOT / "certs" / "idp.crt"),
     "service": {
@@ -15,11 +15,11 @@ CONFIG: dict[str, object] = {
             "endpoints": {
                 "single_sign_on_service": [
                     (
-                        "http://localhost:9000/sso",
+                        "https://idp-latest.onrender.com/sso",
                         BINDING_HTTP_REDIRECT,
                     ),
                     (
-                        "http://localhost:9000/sso",
+                        "https://idp-latest.onrender.com/sso",
                         BINDING_HTTP_POST,
                     ),
                 ],
