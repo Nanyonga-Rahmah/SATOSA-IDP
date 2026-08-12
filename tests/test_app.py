9fdda9613d838d93c1d18d32df700bafb83021b0
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2026-08-12 13:20:01 +0300

    test: test if idp authenticates users

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,5 +13,18 @@ def test_idp_is_configured_correctly():
     assert server.config.key_file == CONFIG["key_file"]
     assert server.config.cert_file == CONFIG["cert_file"] 
 
+def test_authenticate_valid_user():
+    response =authenticate_user("rahmah", "password123")
 
+    assert response is not None
+
+def test_authenticate_invalid_user():
+    response = authenticate_user("rahmah", "wrong")
+
+    assert response is None
+
+def test_authenticate_unknown_user():
+    response = authenticate_user("nobody", "anything")
+
+    assert response is None    
     
