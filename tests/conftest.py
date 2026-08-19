817ddae3a97fb3448b00d7445389eb4eb0291c77
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2026-08-19 11:00:55 +0300

    test: make the mock idp server a fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@
 import pytest
 
 from idp.app import app as flask_app
-
+from idp.app import create_saml_server
 
 @pytest.fixture
 def client():
@@ -11,3 +11,16 @@ def client():
     flask_app.config.update({"TESTING": True})
     with flask_app.test_client() as client:
         yield client
+
+@pytest.fixture
+def server():
+    return create_saml_server()
+
+
+
+
+
+
+
+    
+
--- tests/test_app.py
+++ tests/test_app.py
@@ -8,13 +8,12 @@ from pytest import mark
 VALID_SAML_REQUEST = "fVHLTsMwEPwVy/cQJ6LQrpJIgQqoVETUhB56M6lLLSV28W54fT1OykNIVY47mtmZnU0MCsg72puVeukUEntvG4Pg4ZR3zoCVqP0oW4VANZT5/RLiMwEHZ8nWtuG/gmhcIBGVI20NZ4t5yvU2aMVs7fJd/Da5dctN/DnnbK0cekrKvcLzEDu1MEjSkIdEfBGIaRCdV5GAyQyiyw1nc59ZG0mDak90gDBsbC2bvUWCmRAiRLScFd95r7TZavM8nvXpSEK4q6oiKB7KirP8J/+1Ndi1ypXKvepaPa6WJ4ynvbGskWeJbwaGSxy7sa6VNO7dI76c3UAFZUjTB89OGiTh3+6sH/6/MvsC"
 INVALID_SAML_REQUEST = "fake-request"
 
-def test_idp_uses_configured_entity_id():
-    server = create_saml_server()
-
+def test_idp_uses_configured_entity_id(server):
+   
     assert server.config.entityid == CONFIG["entityid"]
 
-def create_valid_saml_response():
-    server = create_saml_server()
+def create_valid_saml_response(server):
+  
 
     parsed_request = server.parse_authn_request(
         VALID_SAML_REQUEST,
@@ -35,12 +34,10 @@ def create_valid_saml_response():
         **sp_info,
     )
 
-    print("Created SAML Response:", response)
 
     return response    
 
-def test_idp_is_configured_correctly():
-    server = create_saml_server()
+def test_idp_is_configured_correctly(server):
 
     assert server.config.key_file == CONFIG["key_file"]
     assert server.config.cert_file == CONFIG["cert_file"] 
@@ -91,8 +88,7 @@ def test_sso_validates_saml_requests(client, saml_request, expected_value)->None
     response = client.get("/sso", query_string={"SAMLRequest": saml_request})
     assert response.status_code == expected_value
 
-def test_idp_creates_response_args_for_authn_request():
-    server = create_saml_server()
+def test_idp_creates_response_args_for_authn_request(server):
 
     parsed_request = server.parse_authn_request(
         VALID_SAML_REQUEST
@@ -134,42 +130,7 @@ def test_sso_route_does_not_create_session_for_invalid_requests(client) -> None:
         query_string={"SAMLRequest": INVALID_SAML_REQUEST},
     )
     assert response.status_code == 400
-
-# def test_login_route_authenticates_a_user_and_creates_response_for_valid_credentials(client) -> None:
-#     """A successful login should return a 200 response and create a SAML response."""
-#     server = create_saml_server()
-
-#     parsed_request = server.parse_authn_request(
-#         VALID_SAML_REQUEST,
-#     )
-
-#     authn_request = parsed_request.message
-
-#     sp_info = server.response_args(authn_request)
-
-#     with client.session_transaction() as session:
-#         session["saml_request"] = VALID_SAML_REQUEST
-#         session["relay_state"] = "dummy"
-#         session["sp_info"] = sp_info
-
-#     response = client.post(
-#         "/login",
-#         data={
-#             "username": "rahmah",
-#             "password": "password123",
-#         },
-#     )
-
-#     assert response.status_code == 200
-
-#     response_data = response.data.decode()
-
-#     print("Response data:", response_data)
-
-#     assert "Response" in response_data
-#     assert sp_info["in_response_to"] in response_data
-#     assert sp_info["sp_entity_id"] in response_data
-#     assert sp_info["destination"] in response_data   
+  
 
 def test_login_route_returns_401_for_invalid_credentials(client) -> None:    
     """An unsuccessful login should return a 401 response and not create a SAML response."""
